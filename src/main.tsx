1155deea71670ae17ce2f5dba6630ba9f112c3be
gvnrpro <guvnorintel@gmail.com>
2025-08-15 22:13:34 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async'; // <-- IMPORT THIS
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const AppWrapper: React.FC = () => {
+  const [queryClient] = React.useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+        retry: 1,
+      },
+    },
+  }));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Sonner />
+        <App />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* WRAP YOUR ENTIRE APP WITH HelmetProvider */}
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AppWrapper />
     </HelmetProvider>
   </React.StrictMode>
 );
